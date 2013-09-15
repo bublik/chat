1532347b800a1dfe4ce2ef0ad16602bec79561d7
Ruslan Voloshin <rebisall@gmail.com>
2013-09-15 09:27:13 +0300

    added factory, customize first page

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :admin do
+  factory :user do
+    name "MyString"
+    password "MyString"
   end
 end
