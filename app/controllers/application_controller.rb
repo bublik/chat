12babb0daaa09273d84519107430ec996fb2be5a
Ruslan Voloshin <rebisall@gmail.com>
2013-09-16 16:48:42 +0300

    Added some models localization and set locale for mane domain as RU and en.helperchat.com for EN locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
+  before_action :set_locale
   before_filter :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token, if: :json_request?
   layout :authorized_layout
 
   protected
+
+  def set_locale
+    I18n.locale = params[:locale] || extract_locale_from_subdomain || extract_locale_from_accept_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password) }
@@ -22,6 +29,21 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def extract_locale_from_accept_language_header
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
+  # Get locale code from request subdomain (like http://it.application.local:3000)
+  # You have to put something like:
+  #   127.0.0.1 ru.application.local
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_s.to_sym) ? parsed_locale : nil
+  end
+
 
   def authorized_layout
     agent_signed_in? ? 'management' : 'application'
