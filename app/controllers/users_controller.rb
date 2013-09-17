352b24bbe03c47aba853a6705d8b67d64b363cb5
Ruslan Voloshin <rebisall@gmail.com>
2013-09-17 10:01:14 +0300

    added localization for jabber agent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: t('.updated_successful')}
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
