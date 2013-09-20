cf5294fd4cd83470802eae74ec1f7db049029a02
Ruslan Voloshin <rebisall@gmail.com>
2013-09-20 09:09:54 +0300

    Added Google Analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,16 @@ module ApplicationHelper
   def icon_boolean(flag = false)
     flag ? icon('icon-ok') : icon('icon-remove')
   end
+
+  def google_analytix
+    "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44192749-1', 'helperchat.com');
+  ga('send', 'pageview');
+</script>"
+  end
 end
