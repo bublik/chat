cce872da91fbe92c51d825308df0b1294235e450
Ruslan Voloshin <rebisall@gmail.com>
2013-09-24 09:14:33 +0300

    updated shema

--- spec/models/archive_jid_pref_spec.rb
+++ spec/models/archive_jid_pref_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: archive_jid_prefs
+#
+#  us            :string(2047)     not null
+#  with_user     :string(1023)     not null
+#  with_server   :string(1023)     not null
+#  with_resource :string(1023)     not null
+#  save          :integer
+#  expire        :integer
+#  otr           :integer
+#
+
 require 'spec_helper'
 
 describe ArchiveJidPref do
