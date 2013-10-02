53d6a4c007c1c4355763543b6a6f3c61a0637082
Ruslan Voloshin <rebisall@gmail.com>
2013-10-02 15:17:18 +0300

    clear mistake files

--- app/assets/javascripts/widget/Flocktory_files/top_advocates.js
+++
@@ -1,9 +0,0 @@
-$('#top_advocates').each(function () {
-  var container = $(this);
-
-  new h.DateFilter($('#date_filter'));
-
-  container.find('.row').click(function () {
-    $(this).parent().toggleClass('open');
-  });
-});
