df398e8e1d38449c2e235f0521a71c0dbca7af02
Ruslan Voloshin <rebisall@gmail.com>
2013-10-23 11:40:04 +0300

    updated mode config

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,5 +1,5 @@
 PayPal::Recurring.configure do |config|
-  config.sandbox = true
+  config.sandbox = APP_CONFIG['paypal']['mode'].eql?('sandbox')
   config.username = APP_CONFIG['paypal']['username']
   config.password = APP_CONFIG['paypal']['password']
   config.signature = APP_CONFIG['paypal']['signature']
