8dbd8d14829e1bb15008c91d98369673a578d2d3
Ruslan Voloshin <rebisall@gmail.com>
2013-10-25 19:02:08 +0300

    Updated style

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,7 +3,7 @@ module LocationsHelper
   def show_map(location = nil)
     return unless location
     image_tag("http://maps.google.com/maps/api/staticmap?size=250x250&sensor=false&zoom=10&markers=#{location.latitude}%2C#{location.longitude}",
-              class: 'round',
+              class: 'round h250',
               style: 'margin-left: -32px')
   end
 
