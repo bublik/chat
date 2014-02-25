78090136f0ae12fbfc93102bed2af0b25602691a
Max Coder <mckenn@gmail.com>
2014-02-25 11:05:18 +0200

    small fixes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = APP_CONFIG['spripe']['api_key']
-STRIPE_PUBLIC_KEY = APP_CONFIG['spripe']['public_key']
+Stripe.api_key = APP_CONFIG['stripe']['api_key']
+STRIPE_PUBLIC_KEY = APP_CONFIG['stripe']['public_key']
